0bc8e0e635e8adf6e4f1a95321dbad43807f0889
Johan Lindeque <johanlindeque34@gmail.com>
2026-01-24 11:28:48 +0200

    setup entityframework

--- BookNest/Program.cs
+++ BookNest/Program.cs
@@ -1,3 +1,6 @@
+using BookNest.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookNest;
 
 public class Program
@@ -5,6 +8,12 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // db context
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BookNestDB"));
+        });
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
@@ -25,9 +34,7 @@ public class Program
         app.UseAuthorization();
 
         app.MapStaticAssets();
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}")
+        app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
             .WithStaticAssets();
 
         app.Run();
