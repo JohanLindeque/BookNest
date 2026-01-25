4773ccd99239d59f1e8b01dafdc04e771c35057f
Johan Lindeque <johanlindeque34@gmail.com>
2026-01-25 13:24:56 +0200

    code cleanup

--- BookNest/Models/ViewModels/LibrarianDashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace BookNest.Models.ViewModels;
-
-public class LibrarianDashboardViewModel
-{
-    public IEnumerable<MemberCheckoutViewModel> OverdueCheckouts { get; set; }
-}
--- BookNest/Models/ViewModels/MemberDashboardViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BookNest.Models.ViewModels;
-
-public class MemberDashboardViewModel
-{
-    public IEnumerable<MemberCheckoutViewModel> ActiveCheckouts { get; set; }
-    public IEnumerable<MemberCheckoutViewModel> PastCheckouts { get; set; }
-}
